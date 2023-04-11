05579391ec3fd98712086e07ad3d0c5323793959
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 14:13:18 -0500

    Create a function to remove item

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,17 @@
-const removeFromArray = function(array, number) {
+const removeFromArray = function(array, num1, num2) {
     //assign argument to look for
     //use indexof to go through the array
-    if (array.indexOf(number)) {
-        let index = array.indexOf(number);
-        array.splice(index, 1);
+    function removeItem(item) {
+        if (array.indexOf(item)) {
+            let index = array.indexOf(item);
+            array.splice(index, 1);
+        }
     }
+    
+    removeItem(num1)
+    // if (num2 != undefined) {
+
+    // }
     return array;
     //loop through array to find the argument
     
