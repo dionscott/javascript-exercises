563dda7b5cf6a85c85cc92424cad18492dbf9620
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 14:07:31 -0500

    Add function paramaters to remove item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, number) {
+    //assign argument to look for
+    //use indexof to go through the array
+    if (array.indexOf(number)) {
+        let index = array.indexOf(number);
+        array.splice(index, 1);
+    }
+    return array;
+    //loop through array to find the argument
+    
+
+    //if found remove item and return array
+
+    //if not, return original array
 
 };
 
