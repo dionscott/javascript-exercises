9a0aa5bb56c5bb79287e07c4bd3320a79133bdbf
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 18:45:16 -0500

    Add functions for add, sum, subtract

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,26 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sum = num1 + num2;
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let diff = num1 - num2;
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  //finds length of array
+  let length = array.length - 1;
+	for (let i = length; i >= 0; i--) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum = 0;
+  //takes an array and multiplies them by each other
 
 };
 
