5f74137cfc06e8f48c7efbcdfdea072435901913
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 13:40:24 -0500

    Add function for reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let seperatedString = string.split("")
+    let backwards = [];
+    for (let i = seperatedString.length; i > 0; i--) {
+        backwards += seperatedString.pop();
+    }
+    return backwards.toString();
 };
 
 // Do not edit below this line
