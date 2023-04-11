c9b0afdf910804055c16c02f8ada3adf7962c7ef
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 16:10:54 -0500

    Add F and C conversion and checks if the return value is a whole number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let fahrenheit = 0;
+  fahrenheit = (fahr - 32) * (5/9);
+  //checks if this is a whole number
+  if (fahrenheit === Math.floor(fahrenheit)) {
+    return fahrenheit
+  }
+  return fahrenheit.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let celsius = 0;
+  celsius = ((cels - 32) * (5/9));
+  //checks if this is a whole number
+  if (celsius === Math.floor(celsius)) {
+    return fahrenheit
+  }
+  return celsius.toFixed(1);
 };
 
 // Do not edit below this line
