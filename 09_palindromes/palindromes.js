042e8a7778fbf803bdbbb451586ffc6551dfb86b
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 19:33:13 -0500

    Add "." to be excluded from simpleString

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
     //remove punctuation and merge string
-    let simpleString = string.replace(/[" ",!]/g, "").toLowerCase();
+    let simpleString = string.replace(/[" ",!.]/g, "").toLowerCase();
     //find the reverse of the string
     let reverse = simpleString.split("").reverse().join("");
 
