3acb743ce18d0284c78531748671eba3262120e7
Dion Scott <dionanthonyscott@gmail.com>
2023-04-11 19:21:44 -0500

    Create function to simplify string and reverse

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //remove punctuation and merge string
+    let simpleString = string.replace(/[" ",!]/g, "").toLowerCase();
+    //find the reverse of the string
+    let reverse = simpleString.split("").reverse().join("");
+
+    //check if it matches the reverse
+    if (simpleString == reverse) {
+        return true;
+    }
+    return false;
+    //return true if yes and false if no
 
 };
 
